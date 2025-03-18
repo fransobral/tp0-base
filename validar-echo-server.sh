8502228bdde2394650692c596fd1e80f861496d5
fransobral <fsobral@fi.uba.ar>
2025-03-18 15:52:24 -0300

    Problema de sintaxis

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,7 +6,7 @@ TIMEOUT=5
 
 RESULT=$(docker run --rm --network tp0_testing_net busybox sh -c "echo '$MESSAGE' | nc -w $TIMEOUT server 12345")
 
-if [ "$RESULT" == "$MESSAGE" ]; then
+if [ "$RESULT" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
